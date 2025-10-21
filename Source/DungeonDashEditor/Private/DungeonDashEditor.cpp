2f6af044e1cb0f8bd0127c6b55fd4a58c1a035ad
Mia <miasedgewick@gmail.com>
2025-10-20 17:20:31 -0700

    Fix Version Control Error
    + Re-added lost crucial files for host project

---
+++ Source/DungeonDashEditor/Private/DungeonDashEditor.cpp
@@ -0,0 +1,24 @@
+﻿#include "DungeonDashEditor.h"
+
+#include "ContentBrowserModule.h"
+#include "LevelEditor.h"
+#include "MenuExtensions/ContentBrowserMenuExtension.h"
+
+#define LOCTEXT_NAMESPACE "FDungeonDashEditorModule"
+DEFINE_LOG_CATEGORY(LogDungeonDashEditor)
+
+void FDungeonDashEditorModule::StartupModule()
+{
+	ContentBrowserMenuExtension = MakeShareable(new FContentBrowserMenuExtension());
+	
+}
+
+void FDungeonDashEditorModule::ShutdownModule()
+{
+    
+}  
+
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FDungeonDashEditorModule, DungeonDashEditor)
---
+++ Source/DungeonDashEditor/Public/DungeonDashEditor.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "MenuExtensions/ContentBrowserMenuExtension.h"
+#include "Modules/ModuleManager.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogDungeonDashEditor, All, All)
+
+class FDungeonDashEditorModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+     
+    UPROPERTY()
+    TSharedPtr<FContentBrowserMenuExtension> ContentBrowserMenuExtension;
+};
